c1a2e78c41c3bd03885007878ac511a1f1340f35
Hansiyuan131 <422295068@qq.com>
2024-03-09 02:13:53 +0800

    feat: add core module

---
+++ xrpc-core/src/main/java/com/sylvan/xrpc/core/XrpcCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sylvan.xrpc.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XrpcCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XrpcCoreApplication.class, args);
+    }
+
+}
---
+++ xrpc-core/src/test/java/com/sylvan/xrpc/core/XrpcCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylvan.xrpc.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XrpcCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
