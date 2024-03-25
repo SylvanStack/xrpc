5de25edef92cab96bf1dd1984939656e28386d1d
Hansiyuan131 <422295068@qq.com>
2024-03-25 23:33:10 +0800

    feat: add test ZookeeperServer

--- xrpc-demo/xrpc-demo-consumer/src/test/java/com/yuanstack/xrpc/demo/consumer/XrpcDemoConsumerApplicationTests.java
+++ xrpc-demo/xrpc-demo-consumer/src/test/java/com/yuanstack/xrpc/demo/consumer/XrpcDemoConsumerApplicationTests.java
@@ -1,13 +1,39 @@
 package com.yuanstack.xrpc.demo.consumer;
 
+import com.yuanstack.xrpc.core.test.TestZookeeperServer;
+import com.yuanstack.xrpc.demo.provider.XrpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:application.yml")
 class XrpcDemoConsumerApplicationTests {
+
+    static ApplicationContext context;
+    static TestZookeeperServer testZookeeperServer;
+
+
+    @BeforeAll
+    static void init() {
+        testZookeeperServer = new TestZookeeperServer();
+        testZookeeperServer.start();
+        context = SpringApplication.run(XrpcDemoProviderApplication.class,
+                "--server.port=8084", "--logging.level.root=debug", "--xrpc.zkServers=localhost:2182");
+    }
 
     @Test
     void contextLoads() {
+        System.out.println("Unit tests");
     }
 
+    @AfterAll
+    static void destroy() {
+        testZookeeperServer.stop();
+        SpringApplication.exit(context, () -> 1);
+    }
 }
